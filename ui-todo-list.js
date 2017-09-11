05c1c2cb0b423cb498e36f4b80d25f8f1a7a6f7d
Neha <nehadeshpande0204@gmail.com>
2017-09-11 15:36:24 -0400

    Text box Validated Issue

--- ui-todo-list.js
+++ ui-todo-list.js
@@ -1,26 +1,27 @@
 
-   //Create a div to contain list of tasks
-    
-    var toDoList = document.createElement("div");
-    toDoList.id = "to-do-list";
-    var toDoTask = document.createElement('ul'); 
-    var txtBoxValidation = document.createElement("div");
-    txtBoxValidation.className="validationMessage";
-    var mainDiv;
-    //Add Listbox in the div along with UL element
-    window.onload = function (){
+//Create a div to contain list of tasks
 
-        mainDiv = document.getElementById('app');
-        mainDiv.appendChild(txtBoxValidation);
-        mainDiv.appendChild(toDoList);
-        toDoTask.id = 'to-do-task';
-        toDoList.appendChild(toDoTask);
-    }
-    var chkBoxID = 0;
-    
-    //onclick of add
-    function addToList() {
+var toDoList = document.createElement("div");
+toDoList.id = "to-do-list";
+var toDoTask = document.createElement('ul'); 
+var txtBoxValidation = document.createElement("div");
+txtBoxValidation.className="validationMessage";
+var mainDiv;
+//Add Listbox in the div along with UL element
+window.onload = function (){
 
+    mainDiv = document.getElementById('app');
+    mainDiv.appendChild(txtBoxValidation);
+    mainDiv.appendChild(toDoList);
+    toDoTask.id = 'to-do-task';
+    toDoList.appendChild(toDoTask);
+}
+var chkBoxID = 0;
+
+//onclick of add
+function addToList() {
+
+    if(validateForm()){
         //create new List element
         var listElement = document.createElement('li');
         listElement.id="myList";
@@ -34,7 +35,7 @@
         var chklistVal = document.createTextNode(document.getElementById('txtAdd').value);
         var label1=document.createElement("label");
         label1.appendChild(chklistVal);
-    
+
         //create checkListTask
         listElement.appendChild(chkBoxList);
         listElement.appendChild(label1);
@@ -46,5 +47,22 @@
             else
                 label1.style="tetext-decoration-line: none";
         }
+        txtBoxValidation.style="visibility:hidden";
     }
-   
+}
+
+function validateForm(){
+
+    //Text Box Validate
+    var value = document.getElementById('txtAdd').value;
+    var msg;
+    if(value == ""){
+        msg  =  document.createTextNode("Please provide input into the task List");
+        var label = document.createElement('label');
+        label.appendChild(msg);
+        txtBoxValidation.appendChild(label);
+        txtBoxValidation.style="visibility:visible"
+        return false;
+    }
+    return true;
+}
